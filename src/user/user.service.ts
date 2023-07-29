dbd319e354e577e75551d006889cdffdbcee98a6
ivan.hryshko <ihryshko@datrics.ai>
2023-07-29 23:01:00 +0300

    feat: added UserResponseInterface

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,6 +5,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { sign } from "jsonwebtoken";
 import { JWT_SECRET } from "@app/config";
+import { UserResponseInterface } from "./types/userResponse.interface";
 
 @Injectable()
 export class UserService {
@@ -28,7 +29,7 @@ export class UserService {
     }, JWT_SECRET)
   }
 
-  buildUserResponse(user: UserEntity): any {
+  buildUserResponse(user: UserEntity): UserResponseInterface {
     return {
       user: {
         ...user,
