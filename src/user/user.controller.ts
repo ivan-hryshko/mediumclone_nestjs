13c04f44105d5cf7651ee5b3a9dbd2fb20574db8
ivan.hryshko <ihryshko@datrics.ai>
2023-07-30 15:46:34 +0300

    fix: import request

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,18 @@
-import { Body, Controller, Get, Injectable, Post, Req, UsePipes, ValidationPipe } from "@nestjs/common";
-import { UserService } from "@app/user/user.service";
-import { CreateUserDto } from "./dto/createUser.dto";
-import { UserResponseInterface } from "./types/userResponse.interface";
-import { LoginUserDto } from "./dto/loginUser.dto";
-import { UserEntity } from "./user.entity";
-// import { ExpressRequest } from "@app/types/expressRequest.interface";
-
-export class ExpressRequest extends Request {
-  user?: UserEntity
-  // headers: HeadersWithToken;
-}
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UserService } from '@app/user/user.service';
+import { CreateUserDto } from './dto/createUser.dto';
+import { UserResponseInterface } from './types/userResponse.interface';
+import { LoginUserDto } from './dto/loginUser.dto';
+import { Request } from 'express';
+import { ExpressRequest } from '@app/types/expressRequest.interface';
 
 @Controller()
 export class UserController {
@@ -17,23 +20,27 @@ export class UserController {
 
   @Post('users')
   @UsePipes(new ValidationPipe())
-  async createUser(@Body('user') createUserDto: CreateUserDto): Promise<UserResponseInterface> {
-    const user = await this.userService.createUser(createUserDto)
-    return this.userService.buildUserResponse(user)
+  async createUser(
+    @Body('user') createUserDto: CreateUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.createUser(createUserDto);
+    return this.userService.buildUserResponse(user);
   }
 
   @Post('users/login')
   @UsePipes(new ValidationPipe())
-  async loginUser(@Body('user') loginUserDto: LoginUserDto): Promise<UserResponseInterface> {
-    const user = await this.userService.loginUser(loginUserDto)
-    return this.userService.buildUserResponse(user)
+  async login(
+    @Body('user') loginDto: LoginUserDto,
+  ): Promise<UserResponseInterface> {
+    console.log('loginDto', loginDto);
+    const user = await this.userService.loginUser(loginDto);
+    return this.userService.buildUserResponse(user);
   }
 
-  // @Injectable()
   @Get('user')
-  async currentUser(@Req() request:ExpressRequest): Promise<UserResponseInterface> {
-    console.log('user', request.user)
-    return 'currentUser' as any
+  async currentUser(
+    @Req() request: ExpressRequest,
+  ): Promise<UserResponseInterface> {
+    return this.userService.buildUserResponse(request.user);
   }
-
 }
