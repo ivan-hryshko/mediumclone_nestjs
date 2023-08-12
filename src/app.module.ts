0b75cde99d627faf49fa86fd7117d10a1344e811
ivan.hryshko <ihryshko@datrics.ai>
2023-08-12 16:31:07 +0300

    feat: added Article Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import ormconfig from '@app/ormconfig';
 import { UserModule } from '@app/user/user.module';
 import { AuthMiddleWare } from './user/middlewares/auth.middleware';
+import { ArticleModule } from './article/article.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(ormconfig),
     TagModule,
     UserModule,
+    ArticleModule,
   ],
   controllers: [AppController],
   providers: [AppService],
